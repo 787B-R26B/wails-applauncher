e637759d5911cb8488693e4b127861e559dac2d8
787B-R26B <s.tomoyasu.sa22c@gmail.com>
2025-10-20 04:54:08 +0900

    add: do ls -l command button

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os/exec"
+	"strings"
 )
 
 // App struct
@@ -24,4 +26,17 @@ func (a *App) startup(ctx context.Context) {
 // Greet returns a greeting for the given name
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+func (a *App) ExecuteCommand(command string) (string, error) {
+	parts := strings.Split(command, " ")
+	head := parts[0]
+	parts = parts[1:]
+
+	cmd := exec.Command(head, parts...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(output), err
+	}
+	return string(output), nil
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,34 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useState } from "react";
+import logo from "./assets/images/logo-universal.png";
+import "./App.css";
+import { ExecuteCommand } from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+  const [resultText, setResultText] = useState("click the button.");
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  function handleExecute() {
+    ExecuteCommand("ls -l")
+      .then((result) => {
+        setResultText(result);
+      })
+      .catch((err) => {
+        setResultText(err);
+      });
+  }
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+  return (
+    <div id="App">
+      <img src={logo} id="logo" alt="logo" />
+      <h1>wails-react-launcher</h1>
+      <div className="input-box">
+        <button className="btn" onClick={handleExecute}>
+          Execute 'ls -l'
+        </button>
+      </div>
+      <pre id="result" className="result">
+        {resultText}
+      </pre>
+    </div>
+  );
 }
-
-export default App
+export default App;
